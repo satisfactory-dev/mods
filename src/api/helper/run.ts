9ba530a67b4d589528dcd1f4f1571ed397a1d959
SignpostMarv <me@signpostmarv.name>
2026-07-30 18:33:54 +0000

    tweak to make it easier to debug failures

--- src/api/helper/run.ts
+++ src/api/helper/run.ts
@@ -1,4 +1,10 @@
 export default (operation: string, sub_query: string): Promise<unknown> => {
+	const body = {
+		query: `{${operation} {
+			${sub_query}
+		}}`,
+	};
+
 	return fetch(
 		'https://api.ficsit.app/v2/query',
 		{
@@ -6,11 +12,7 @@ export default (operation: string, sub_query: string): Promise<unknown> => {
 			headers: {
 				'Content-Type': 'application/json',
 			},
-			body: JSON.stringify({
-				query: `{${operation} {
-					${sub_query}
-				}}`,
-			}),
+			body: JSON.stringify(body),
 		},
 	).then((res) => {
 		return res.json();
