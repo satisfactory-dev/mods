4be8efa6137b0462d86ecd1fce97c3e534403ce4
SignpostMarv <me@signpostmarv.name>
2026-08-12 09:29:24 +0000

    integrity hashes on inline scripts

--- populate-html.ts
+++ populate-html.ts
@@ -1,4 +1,8 @@
 import {
+	hash,
+} from 'node:crypto';
+
+import {
 	basename,
 	dirname,
 } from 'node:path';
@@ -95,47 +99,18 @@ for (const key of Object.keys(files)) {
 
 const now = new Date();
 
-const index = html`<html
-	lang="en"
->
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1" />
-<title>Mods</title>
-<meta
-	name="description"
-	content="${
-		'An experimental forked search alternative of https://ficsit.app/mods.'
-	}"
->
-<base href="/">
-${[...modulepreloads].map((filename) => html`
-<link
-	rel="modulepreload"
-	as="script"
-	href="./js/${filename}"
-	crossorigin="anonymous"
->
-`)}
-${sources.map((source) => html`<link
-	rel="preload"
-	as="fetch"
-	href="${source}"
-	crossorigin="anonymous"
->`)}
-<link rel="stylesheet" href="./css/${files.style}">
-</head>
-<body>
-<script type="importmap">${unsafeStatic(stringify({
+const import_map = stringify({
 	imports: {
 		'./js/ui.js': `./js/${files.ui}`,
 		'./js/provider.js': `./js/${files.provider}`,
 		'./js/web.js': `./js/${files.web}`,
 		'./js/thread.js': `./js/${files.thread}`,
 	},
-}))}</script>
-<script type="module">
-import init from './js/ui.js';
+});
+
+const import_map_hash = hash('sha256', import_map, 'base64');
+
+const init = `import init from './js/ui.js';
 import Provider from './js/provider.js';
 
 let params = new URLSearchParams(location.search);
@@ -156,8 +131,50 @@ document.addEventListener('DOMContentLoaded', () => {
 			provider,
 		});
 	});
-});
-</script>
+});`;
+
+const init_hash = hash('sha256', init, 'base64');
+
+const index = html`<!DOCTYPE html>
+<html
+	lang="en"
+>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<title>Mods</title>
+<meta
+	name="description"
+	content="${
+		'An experimental forked search alternative of https://ficsit.app/mods.'
+	}"
+>
+<base href="/">
+<script
+	type="importmap"
+	integrity="sha256-${import_map_hash}"
+>${unsafeStatic(import_map)}</script>
+${[...modulepreloads].map((filename) => html`
+<link
+	rel="modulepreload"
+	as="script"
+	href="./js/${filename}"
+	crossorigin="anonymous"
+>
+`)}
+${sources.map((source) => html`<link
+	rel="preload"
+	as="fetch"
+	href="${source}"
+	crossorigin="anonymous"
+>`)}
+<link rel="stylesheet" href="./css/${files.style}">
+</head>
+<body>
+<script
+	type="module"
+	integrity="sha-256,${init_hash}"
+>${unsafeStatic(init)}</script>
 <main></main>
 <footer>
 	<p>An experimental forked search alternative of <a
